dc61ec56213ce0609741ad6d2cc2ba0b241ed048
Huzmaan <princehuzmaanpasta@gmail.com>
2021-10-28 23:34:52 +0500

    first Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="Header-css">
+            <a href="#" className="Header-text">HOME</a>
+            <a href="#About" className="Header-text" >ABOUT</a>
+            <a href="#Menu" className="Header-text">MENU</a>
+            <a href="#Where" className="Header-text">WHERE</a>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className="Main-css">
+            <div>
+                <p className="Main-text">Open from 6am to 5pm</p>
+            </div>
+            <div className="Main" >
+                <h1 className="Main-text1">
+                    the
+                    <br/>
+                    Cafe
+                </h1>
+            </div>
+            <div >
+                <p className="Main-text2">15 Adr street, 5015</p>
+            </div>
+        </div>
+    )
+}
+
+export default Main
